50deb7ba2de176ec9b58f3ec13edec119a3d7dd1
Pedro Costa <pedrohefc@gmail.com>
2024-07-10 08:46:25 -0300

    first commit

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
 const form = document.getElementById('form-banco');
 
-function maiorMenor(p){
-    subtracao = p - d
-    return subtracao > 0;
-}
-
 form.addEventListener('submit', function(e){
     e.preventDefault();
 
-    const dividaAtual = document.getElementById('divida');  
-    const patrimonio = document.getElementById('patrimonio');
-    if (maiorMenor (patrimonio.value, dividaAtual.value) === true) {
-        alert('ola')
+    const nome = document.getElementById('nome-pessoa');
+    const dividaAtual = document.getElementById('divida-pessoa');  
+    const patrimonio = document.getElementById('patrimonio-pessoa');
+
+    if(dividaAtual.value > patrimonio.value){
+        alert('Sua divida atual é maiorque seu patriomonio! Crédito negado!')
+    } else{
+        alert('Podemos prosseguir com sua análise de crédito!')
     }
+
+    dividaAtual.value = ""
+    patrimonio.value = ""
+    nome.value = ""
 })
-
-console.log(form)
-
